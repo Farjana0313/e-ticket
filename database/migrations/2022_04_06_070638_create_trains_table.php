f2b3724709a3015707a514c6c8b5bbadb829a9ed
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2022-04-06 15:11:34 +0600

    Initial commit

---
+++ database/migrations/2022_04_06_070638_create_trains_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTrainsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('trains', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->uniqid();
+            $table->date('date');
+            $table->unsignedInteger('home_station_id');
+            $table->unsignedInteger('start_time');
+            $table->timestamps();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('trains');
+    }
+}
